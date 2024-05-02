ed32b79600b6a957ec54be5a8d44b8ce4abb96b1
Aksh Sharma <aks>
2024-05-01 21:37:55 -0400

    Fresh Start with Svelete

---
+++ svelte/src/static/navbar.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let activePage = '';
+
+    const pages = [
+        { name: 'Home', path: '/' },
+        { name: 'About Me', path: '/about' },
+        { name: 'Projects', path: '/projects' }
+    ];
+</script>
+
+<nav>
+    <div class="logo">
+        <img src="../src/assets/inits.svg" alt="Logo" style="width: 30px; height: 30px;" />
+    </div>
+    <ul class="nav-links">
+        {#each pages as page}
+            <li class={activePage === page.path ? 'active' : ''}>
+                <a href={page.path}>{page.name}</a>
+            </li>
+        {/each}
+    </ul>
+</nav>
+
+<style>
+    /* Add your custom styles here */
+</style>
